19bcb88190ee0b737dacc3427d599b750eac67f8
Vinícius <viniciusmpacheco22@gmail.com>
2023-10-07 13:32:48 -0300

    corrigindo bug carrossel

--- assets/JavaScript/product.js
+++ assets/JavaScript/product.js
@@ -1,39 +1,44 @@
 /*const slider = document.querySelector('div.carrossel')*/
-const slider = document.querySelectorAll('img.thumbs')
-const right = document.getElementById('btn-r')
-const left = document.getElementById('btn-l')
+const slider = document.querySelectorAll('img.thumbs');
+const right = document.getElementById('btn-r');
+const left = document.getElementById('btn-l');
+const bigImg = document.querySelector('div.big-img');
 
-let currentSlide = 0
+let currentSlide = 0;
+
+bigImg.style.backgroundImage = `url(${slider[currentSlide].src})`;
 
 function hideSlider() {
-    slider.forEach(item => item.classList.remove('on'))
+  slider.forEach((item) => item.classList.remove('on'));
 }
 
 function showSlider() {
-    slider[currentSlide].classList.add('on')
+  slider[currentSlide].classList.add('on');
 }
 
-function nextSlider(){
-    hideSlider()
-    if(currentSlide === slider.length -1) {
-        currentSlide = 0
-    } else {
-        currentSlide++
-    }
-    showSlider()
+function nextSlider() {
+  hideSlider();
+  if (currentSlide === slider.length - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+
+  bigImg.style.backgroundImage = `url(${slider[currentSlide].src})`;
+  showSlider();
 }
 
-function prevSlider(){
-    hideSlider()
-    if(currentSlide === 0){
-        currentSlide = slider.length -1
-    } else {
-        currentSlide--
-    }
+function prevSlider() {
+  hideSlider();
+  if (currentSlide === 0) {
+    currentSlide = slider.length - 1;
+  } else {
+    currentSlide--;
+  }
 
-    showSlider()
+  bigImg.style.backgroundImage = `url(${slider[currentSlide].src})`;
+  showSlider();
 }
 
-console.log(slider)
-left.addEventListener('click', () => prevSlider)
-right.addEventListener('click', () => nextSlider)
+left.addEventListener('click', () => prevSlider());
+right.addEventListener('click', () => nextSlider());
